a67bc688f5727b4ee3be819aece91cfd898d1b72
rouq-alt <iqbal.shorouq@gmail.com>
2025-04-22 23:08:56 +0500

    Create course.h

---
+++ LAB 09/Q3/course.h
@@ -0,0 +1,18 @@
+#ifndef COURSE
+#define COURSE
+
+#include <string>
+using namespace std;
+
+class Course {
+protected:
+    string courseCode;
+    int credits;
+
+public:
+    Course(string code, int credits) : courseCode(code), credits(credits) {}
+    virtual void calculateGrade() = 0;
+    virtual void displayInfo() = 0;
+    virtual ~Course() {}
+};
+#endif
